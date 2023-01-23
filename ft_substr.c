b330168c4d17f59e11b69ca8880eccfb7418d591
wmougharbel <103928495+wmougharbel@users.noreply.github.com>
2023-01-23 16:25:38 +0200

    Add files via upload

---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmoughar <wmoughar@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/26 13:57:36 by wmoughar          #+#    #+#             */
+/*   Updated: 2022/12/26 13:57:36 by wmoughar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc((len + 1) * sizeof(*s));
+	if (!str)
+		return (0);
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
